48f618c4e6d3ba2040b1cbbd6e679cfb2673bfc3
Josh Faigan <djjoker13@gmail.com>
2024-02-02 16:53:18 -0500

    basic ingestion working via insomnia

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Dict, Union
+import httpx
+from initializers.axiom_helper import AxiomHelper
+
+app = FastAPI()
+axiom_helper = AxiomHelper()
+
+class EventData(BaseModel):
+  data: Union[Dict, List[Dict]]
+
+@app.post("/ingest/event/")
+async def ingest_event(event_data: EventData):
+  try:
+    data_to_send = event_data.data if isinstance(event_data.data, list) else [event_data.data]
+    await axiom_helper.send_event(data_to_send)
+    return {"message": "Data ingested successfully!"}
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
---
+++ initializers/axiom_helper.py
@@ -0,0 +1,15 @@
+import axiom
+import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+AXIOM_DATASET = os.getenv("AXIOM_DATASET")
+
+class AxiomHelper:
+    def __init__(self):
+        self.client = axiom.Client(os.getenv("AXIOM_KEY"), AXIOM_DATASET)
+
+    
+    async def send_event(self, event_data):
+        self.client.ingest_events(AXIOM_DATASET, event_data)
