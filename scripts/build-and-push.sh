2e52ddd61b6bca31c4588e9675ddcae651f157d8
Josh Faigan <djjoker13@gmail.com>
2024-02-08 15:47:40 -0500

    again

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Ensure we're in the directory containing the docker-compose.yml file
+# Root directory of the project
 cd "$(dirname "$0")/.."
 
-# Build the Docker image using docker-compose
+# Build the Docker image using docker-compose and tag it with the latest tag and push it to the registry
 docker-compose build app
 
-# After building, tag the built image appropriately. 
-# The image name typically follows the format "<directory_name>_service_name"
-# but since we want to tag it for pushing to a Docker registry, we specify the tag directly.
 docker tag "dev-build_app" "evilgenius13/starfire:latest"
 
-# Push the tagged image to the Docker registry
 docker push "evilgenius13/starfire:latest"
